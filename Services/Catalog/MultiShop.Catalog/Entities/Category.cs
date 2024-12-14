ee013888bf624c03d5e7892ca23b09ab27409521
Muhammed <Muhammed@SAYRAC>
2024-12-14 15:33:29 +0300

    Entities and Automapper added

--- Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -1,7 +1,15 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class Category
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
 
 
 
