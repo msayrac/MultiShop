02d854ab8cff04172111c3f99daa4fb8f82273c7
msayrac <msayracode@gmail.com>
2024-12-18 19:50:07 +0300

    Catalog Api is created

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Services.CategoryServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryService.GetAllCategoryAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(string id)
+        {
+            var values = _categoryService.GetByIdCategoryAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori Basariyla Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return Ok("Kategori Basariyla Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return Ok("Kategori Basariyla Guncellendi");
+        }
+
+
+
+
+
+
+
+    }
+}
